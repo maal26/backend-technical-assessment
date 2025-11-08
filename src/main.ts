f1aa520d7e5bcc3f351bfb93b72745ce0c75e9c6
Mathews Lima <mathews.limaa@gmail.com>
2025-11-07 21:14:02 -0300

    new file to bootstrap the app

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { envs } from './shared/config/envs.ts';
+
+import authRoutes from './modules/auth/authenticate/routes.ts';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+app.listen(envs.API_PORT, () => {
+    console.log(`server running on port ${envs.API_PORT}`);
+});
