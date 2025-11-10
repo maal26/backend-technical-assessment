6975521cd0c9a34a9d423a14616ab4c6db6d772f
Mathews Lima <mathews.limaa@gmail.com>
2025-11-10 11:09:06 -0300

    compare with a "dummy hash" to prevent timing attack

--- src/modules/auth/authenticate/use-cases.ts
+++ src/modules/auth/authenticate/use-cases.ts
@@ -8,6 +8,8 @@ import { sessionRepository } from "@/shared/database/repository/sessions.ts";
 import { errorResponse, successResponse } from "@/shared/infra/http/api-response.ts";
 import { STATUS_CODES } from "@/shared/infra/http/status-code.ts";
 
+const DUMMY_HASH = "$2b$10$CwTycUXWue0Thq9StjUM0uJ8Yq4eKq8hWw6q9KzQ1G1f6fG6XcO2";
+
 export async function authenticate({ email, password }: AuthenticateUserInput) {
     const { getUserByEmail } = userRepository();
     const { createSession, removeAllUserSessions } = sessionRepository();
@@ -15,6 +17,8 @@ export async function authenticate({ email, password }: AuthenticateUserInput) {
     const user = await getUserByEmail(email);
 
     if (!user) {
+        await bcrypt.compare(password, DUMMY_HASH);
+
         return errorResponse("Invalid Credentials", STATUS_CODES.UNAUTHORIZED);
     }
 
