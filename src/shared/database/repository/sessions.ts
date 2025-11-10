3776a02386fd0bb1f7e3b4ad7f8b08886e2343a6
Mathews Lima <mathews.limaa@gmail.com>
2025-11-10 11:16:55 -0300

    rename variable

--- src/shared/database/repository/sessions.ts
+++ src/shared/database/repository/sessions.ts
@@ -6,12 +6,12 @@ import { and, eq, gt } from "drizzle-orm";
 export const sessionRepository = () => {
     const createSession = async (userId: number) => {
         const token = crypto.randomBytes(32).toString("hex");
-        const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 2);
+        const expiresInTwoHours = new Date(Date.now() + 1000 * 60 * 60 * 2);
 
         await db.insert(sessions).values({
             userId,
             token: token,
-            expiresAt: expiresAt,
+            expiresAt: expiresInTwoHours,
         });
 
         return token;
